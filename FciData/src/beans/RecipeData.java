5c4ff46436ef169794032d9c7b042f5de5974f5c
sanjaymanegit <sanjaymane1610@gmail.com>
2020-08-23 14:27:37 +0530

    updated

---
+++ FciData/src/beans/RecipeData.java
@@ -0,0 +1,48 @@
+package beans;
+
+public class RecipeData {
+
+	private String BatchIdFromDevice;
+	private String RecipeNo;
+	private String Col1Key;
+	private int Col1Value;
+	
+	
+    
+    public String getBatchIdFromDevice() {
+        return BatchIdFromDevice;
+    }
+ 
+    public void setBatchIdFromDevice(String BatchIdFromDevice) {
+        this.BatchIdFromDevice = BatchIdFromDevice;
+    }
+	
+   
+    
+    public String getRecipeNo() {
+        return RecipeNo;
+    }
+ 
+    public void setRecipeNo(String RecipeNo) {
+        this.RecipeNo = RecipeNo;
+    }	
+	
+    public String getCol1Key() {
+        return Col1Key;
+    }
+ 
+    public void setCol1Key(String Col1Key) {
+        this.Col1Key = Col1Key;
+    }
+    
+    public int getCol1Value() {
+        return Col1Value;
+    }
+ 
+    public void setCol1Value(int Col1Value) {
+        this.Col1Value = Col1Value;
+    }
+	
+	
+	
+}
