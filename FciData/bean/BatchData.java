ef71070b5ff569d41b436227c26f5686a9fc6449
sanjaymanegit <sanjaymane1610@gmail.com>
2020-08-24 19:33:01 +0530

    added

---
+++ FciData/bean/BatchData.java
@@ -0,0 +1,174 @@
+package bean;
+
+public class BatchData {
+
+	private String BatchId;
+	private String BatchIdDisplay;
+	private String BatchDt;
+	private String AccpetedWtKg;
+	private String AcceptedBagsCnt;
+	private String ReceivedWtKg;
+	private String ReceivedBagsCnt;
+	private String TLReceived;
+	private String TLAccepted;
+	private String ShortageBags;
+	private String Material;
+	private String Status;
+	private String WagonCnt;
+	private String RequiredTrucks;
+	private String ContractorName;	
+	private String DeviceId;
+	
+    
+    public String getBatchId() {
+        return BatchId;
+    }
+ 
+    public void setBatchId(String BatchId) {
+        this.BatchId = BatchId;
+    }
+	
+    public String getBatchIdDisplay() {
+        return BatchIdDisplay;
+    }
+ 
+    public void setBatchIdDisplay(String BatchIdDisplay) {
+        this.BatchIdDisplay = BatchIdDisplay;
+    }
+    
+    public String getBatchDt() {
+        return BatchDt;
+    }
+ 
+    public void setBatchDt(String BatchDt) {
+        this.BatchDt = BatchDt;
+    }
+    
+    public String getAccpetedWtKg() {
+        return AccpetedWtKg;
+    }
+ 
+    public void setAccpetedWtKg(String AccpetedWtKg) {
+        this.AccpetedWtKg = AccpetedWtKg;
+    }
+    
+    public String getAcceptedBagsCnt() {
+        return AcceptedBagsCnt;
+    }
+ 
+    public void setAcceptedBagsCnt(String AcceptedBagsCnt) {
+        this.AcceptedBagsCnt = AcceptedBagsCnt;
+    }
+	
+    public String getReceivedWtKg() {
+        return ReceivedWtKg;
+    }
+ 
+    public void setReceivedWtKg(String ReceivedWtKg) {
+        this.ReceivedWtKg = ReceivedWtKg;
+    }
+    
+    public String getReceivedBagsCnt() {
+        return ReceivedBagsCnt;
+    }
+ 
+    public void setReceivedBagsCnt(String ReceivedBagsCnt) {
+        this.ReceivedBagsCnt = ReceivedBagsCnt;
+    }
+    
+    public String getTLReceived() {
+        return TLReceived;
+    }
+ 
+    public void setTLReceived(String TLReceived) {
+        this.TLReceived = TLReceived;
+    }
+    
+	 public String getTLAccepted() {
+        return TLAccepted;
+    }
+ 
+    public void setTLAccepted(String TLAccepted) {
+        this.TLAccepted = TLAccepted;
+    }
+    
+    public String getShortageBags() {
+        return ShortageBags;
+    }
+ 
+    public void setShortageBags(String ShortageBags) {
+        this.ShortageBags = ShortageBags;
+    }
+    
+    public String getMaterial() {
+        return Material;
+    }
+ 
+    public void setMaterial(String Material) {
+        this.Material = Material;
+    }
+    
+	
+    public String getStatus() {
+        return Status;
+    }
+ 
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
+    
+	
+	 public String getWagonCnt() {
+        return WagonCnt;
+    }
+ 
+    public void setWagonCnt(String WagonCnt) {
+        this.WagonCnt = WagonCnt;
+    }
+	
+	 public String getRequiredTrucks() {
+        return RequiredTrucks;
+    }
+ 
+    public void setRequiredTrucks(String RequiredTrucks) {
+        this.RequiredTrucks = RequiredTrucks;
+    }
+	
+	
+	 public String getContractorName() {
+        return ContractorName;
+    }
+ 
+    public void setContractorName(String ContractorName) {
+        this.ContractorName = ContractorName;
+    }
+	
+	public String getDeviceId() {
+        return DeviceId;
+    }
+ 
+    public void setDeviceId(String DeviceId) {
+        this.DeviceId = DeviceId;
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
