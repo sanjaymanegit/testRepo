01fd0e3215347c2a1b251da9324e332dbdafdca8
sanjaymanegit <sanjaymane1610@gmail.com>
2020-08-31 19:23:23 +0530

    add

---
+++ FCI/upload_batches_job.sh
@@ -0,0 +1,5 @@
+#######################
+pwd
+echo  "Batches upload started"
+cd /home/pi/FCI
+python3.5 /home/pi/FCI/upload_batches.py
---
+++ FCI/upload_weight_job.sh
@@ -0,0 +1,5 @@
+#######################
+pwd
+echo  "Weight upload started"
+cd /home/pi/FCI
+python3.5 /home/pi/FCI/upload_weights.py
