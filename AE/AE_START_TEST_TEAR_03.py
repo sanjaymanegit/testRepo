ee08acf15af77c89057dff4bc00e87c4d59b5b3c
sanjaymanegit <sanjaymane1610@gmail.com>
2023-08-29 15:11:40 +0530

    ok

--- AE/AE_START_TEST_TEAR_03.py
+++ AE/AE_START_TEST_TEAR_03.py
@@ -2334,6 +2334,9 @@ class PlotCanvas_Auto(FigureCanvas):
         self.break_sence=0
         self.test_motor_speed=0
         self.test_rev_speed=0
+        self.per_test_motor_speed=0
+        self.per_test_rev_speed=0
+        self.max_speed=0
         self.test_guage_mm=0
         self.test_type="Tensile"
         self.max_load=0
@@ -2417,10 +2420,11 @@ class PlotCanvas_Auto(FigureCanvas):
         connection.close()     
         
         connection = sqlite3.connect("tyr.db")
-        results=connection.execute("SELECT GRAPH_SCALE_CELL_2,GRAPH_SCALE_CELL_1,AUTO_REV_TIME_OFF,BREAKING_SENCE from SETTING_MST") 
+        results=connection.execute("SELECT GRAPH_SCALE_CELL_2,GRAPH_SCALE_CELL_1,AUTO_REV_TIME_OFF,BREAKING_SENCE,MOTOR_MAX_SPEED from SETTING_MST") 
         for x in results:
                  self.auto_rev_time_off=int(x[2])
                  self.break_sence=int(x[3])
+                 self.max_speed=int(x[4])
                  print("self.load_unit:"+str(self.load_unit)+"    self.disp_unit:"+str(self.disp_unit))
                  if(self.graph_type=="Load Vs Displacement"):
                          if(self.load_unit=="Kg" and self.disp_unit=="Mm"):
@@ -2470,9 +2474,14 @@ class PlotCanvas_Auto(FigureCanvas):
                          self.axes.set_xlabel('Strain (%)')
                          self.axes.set_ylabel("Stress (MPa)'")                      
                  
-                 self.axes.set_xlim(0,int(x[0]))
-                 self.axes.set_ylim(0,int(x[1]))  
-        connection.close()        
+                 self.axes.set_xlim(0,float(x[0]))
+                 self.axes.set_ylim(0,float(x[1]))  
+        connection.close()
+        
+        self.per_test_speed=float((float(self.test_speed)/float(self.max_speed))*100)
+        self.per_test_rev_speed=float((float(self.test_rev_speed)/float(self.max_speed))*100)
+        self.per_test_speed=self.per_test_speed*100
+        self.per_test_rev_speed=self.per_test_rev_speed*100
         
  ###### Set Modbus register for Test   ##########
 #         self.test_method=1
@@ -2529,7 +2538,7 @@ class PlotCanvas_Auto(FigureCanvas):
                    
                     try:
                         print("\n\n\n\n##### SET : Breaking Sence Or SET LOW ######")
-                        self.instrument.write_float(7,float(self.break_sence),2)
+                        self.instrument.write_float(9,float(self.break_sence),2)
                         #self.instrument.write_register(6,0,0)
                         self.record_modbus_logs(self.test_id,self.cycle_num,"SET","SET Breaking Sence :"+str(self.break_sence),self.login_user_role)
                         #time.sleep(5)
@@ -2541,25 +2550,25 @@ class PlotCanvas_Auto(FigureCanvas):
                     try:
                         print("\n\n\n\n##### SET : test_speed ######")
                         #self.instrument.write_register(REGISTER, NEW_VALUE, DECIMALS, functioncode=6, signed=True)
-                        self.instrument.write_register(10,float(self.test_speed),0,6)
+                        self.instrument.write_register(10,float(self.per_test_speed),0,6)
                         #self.instrument.write_register(6,0,0)
-                        self.record_modbus_logs(self.test_id,self.cycle_num,"SET","SET test_speed :"+str(self.test_speed),self.login_user_role)
+                        self.record_modbus_logs(self.test_id,self.cycle_num,"SET","SET test_speed :"+str(self.per_test_speed),self.login_user_role)
                         #time.sleep(5)
                     except IOError as e:
                             print("Ignore-Modbus Error- self.test_speed.:"+str(e))
-                            self.record_modbus_logs(self.test_id,self.cycle_num,"SET","SET test_speed :"+str(self.test_speed),self.login_user_role)
+                            self.record_modbus_logs(self.test_id,self.cycle_num,"SET","SET test_speed :"+str(self.per_test_speed),self.login_user_role)
                             time.sleep(5)
                     
                     try:
                         print("\n\n\n\n##### SET : test_rev_speed ######")
                         #self.instrument.write_register(REGISTER, NEW_VALUE, DECIMALS, functioncode=6, signed=True)
-                        self.instrument.write_register(11,float(self.test_rev_speed),0,6)
+                        self.instrument.write_register(11,float(self.per_test_rev_speed),0,6)
                         #self.instrument.write_register(6,0,0)
-                        self.record_modbus_logs(self.test_id,self.cycle_num,"SET","SET test_rev_speed :"+str(self.test_rev_speed),self.login_user_role)
+                        self.record_modbus_logs(self.test_id,self.cycle_num,"SET","SET test_rev_speed :"+str(self.per_test_rev_speed),self.login_user_role)
                         #time.sleep(5)
                     except IOError as e:
                             print("Ignore-Modbus Error- self.test_rev_speed.:"+str(e))
-                            self.record_modbus_logs(self.test_id,self.cycle_num,"SET","SET test_rev_speed :"+str(self.test_rev_speed),self.login_user_role)
+                            self.record_modbus_logs(self.test_id,self.cycle_num,"SET","SET test_rev_speed :"+str(self.per_test_rev_speed),self.login_user_role)
                             time.sleep(5)
                     
                     try:
@@ -2841,7 +2850,6 @@ class PlotCanvas_Auto(FigureCanvas):
             
                    
     def plot_grah_only(self,i):
-                self.graph_type="Load Vs Displacement"
                 if(self.graph_type=="Load Vs Displacement"):
                             if(self.load_unit=="Kg" and self.disp_unit=="Mm"):
                                         self.line_cnt.set_data(self.arr_p,self.arr_q)
@@ -3015,8 +3023,8 @@ class PlotCanvas(FigureCanvas):
         for x in results:
               self.last_load_unit=str(x[0])
               self.last_disp_unit=str(x[1])
-              ax.set_xlim(0,int(x[2]))
-              ax.set_ylim(0,int(x[3]))  
+              ax.set_xlim(0,float(x[2]))
+              ax.set_ylim(0,float(x[3]))  
         connection.close()
         
         connection = sqlite3.connect("tyr.db")
