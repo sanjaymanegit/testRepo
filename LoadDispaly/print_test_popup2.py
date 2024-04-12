607887d0d0034162382fb81d1e2b26a95bc6f8bd
sanjaymanegit <sanjaymane1610@gmail.com>
2024-04-12 17:07:25 +0530

    ok

--- LoadDispaly/print_test_popup2.py
+++ LoadDispaly/print_test_popup2.py
@@ -13,23 +13,29 @@ from pdf2image import convert_from_path
 
 
 class PrintDemo(QMainWindow):
-
     def __init__(self):
-        QMainWindow.__init__(self)
-
-        self.setMinimumSize(QSize(192, 128))
+         QMainWindow.__init__(self)
+         #self.setupUi()
+  
+        
+     
         self.setWindowTitle("Print Demo")
 
-        printButton = QPushButton('Print', self)
-        printButton.clicked.connect(self.onPrint)
-        printButton.resize(128, 32)
-        printButton.move(32, 48)
+        self.printButton = QPushButton('Print', self)
+        self.printButton.clicked.connect(self.onPrint)
+        self.printButton.resize(128, 32)
+        self.printButton.move(32, 48)
+   
+
+        
 
     def onPrint(self):
         self.printDialog()
 
     def printDialog(self):
-        filePath, filter = QFileDialog.getOpenFileName(self, 'Open file', '', 'PDF (*.pdf)')
+        #filePath, filter = QFileDialog.getOpenFileName(self, 'Label Print', 'reports', 'PDF (*.pdf)')
+        filePath, filter = QFileDialog.getOpenFileName(self, 'Label Print', 'reports', 'test_report.pdf')
+        print(" filePath : "+str(filePath))
         if not filePath:
             return
         file_extension = os.path.splitext(filePath)[1]
@@ -39,7 +45,7 @@ class PrintDemo(QMainWindow):
             dialog = QPrintDialog(printer, self)
             if dialog.exec_() == QPrintDialog.Accepted:
                 with tempfile.TemporaryDirectory() as path:
-                    images = convert_from_path(filePath, dpi=300, output_folder="D:\code\testRepo\LoadDispaly\reports\xx")
+                    images = convert_from_path(filePath, dpi=300, output_folder=path)
                     painter = QPainter()
                     painter.begin(printer)
                     for i, image in enumerate(images):
@@ -57,5 +63,6 @@ class PrintDemo(QMainWindow):
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     mainWin = PrintDemo()
+    #mainWinx.setupUi()
     mainWin.show()
     sys.exit(app.exec_())
