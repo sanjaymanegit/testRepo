c3dc8b003b5a257339fbcf805067a4325832a4ac
sanjaymanegit <sanjaymane1610@gmail.com>
2024-04-16 14:56:21 +0530

    ok

--- LoadDispaly/vv.py
+++ LoadDispaly/vv.py
@@ -1,6 +1,12 @@
-import tempfile 
-  
-fo = tempfile.NamedTemporaryFile() 
-print(fo.name) 
-  
-fo.close() 
+import usb
+busses = usb.busses()
+for bus in busses:
+  devices = bus.devices
+  for dev in devices:
+    print (str(dev))
+    print ("Device:"+str(dev.filename))
+    #print ("  idVendor: %d (0x%04x)" % (dev.idVendor, dev.idVendor)
+    #print "  idProduct: %d (0x%04x)" % (dev.idProduct, dev.idProduct)
+    print ("Manufacturer:"+str(dev.iManufacturer))
+    #print "Serial:", dev.iSerialNumber
+    #print "Product:", dev.iProduct
