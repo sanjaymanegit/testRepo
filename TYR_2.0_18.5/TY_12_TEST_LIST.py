b5a5a99c1d848645cdbbe1d20239a064c8c2e65d
sanjaymanegit <sanjaymane1610@gmail.com>
2024-07-03 12:34:42 +0530

    added IFD test files

--- TYR_2.0_18.5/TY_12_TEST_LIST.py
+++ TYR_2.0_18.5/TY_12_TEST_LIST.py
@@ -29,6 +29,7 @@ from TY_67_START_UNIRUB_TEAR import TY_67_Ui_MainWindow
 from TY_69_START_UNIRUB_ADHESION import TY_69_Ui_MainWindow
 from TY_71_START_TEST_CLD3 import TY_71_Ui_MainWindow
 from TY_73_START_TEST_RADIAL import TY_73_Ui_MainWindow
+from TY_75_START_TEST_IFD import TY_75_Ui_MainWindow
 
 
 
@@ -493,9 +494,25 @@ class TY_12_LIST_Ui_MainWindow(object):
             self.save_test_cld3()
         elif(str(self.test_type_id) == "37"):  
             self.save_test_radial()
+        elif(str(self.test_type_id) == "38"):  
+            self.save_test_IFD()
         else:
             print("Invalid Test ID"+str(self.test_type_id))
     
+    def save_test_IFD(self):                     
+        connection = sqlite3.connect("tyr.db")              
+        with connection:        
+                    cursor = connection.cursor()
+                    cursor.execute("UPDATE GLOBAL_VAR SET NEW_TEST_NAME='IFD'")                    
+        connection.commit();
+        connection.close()
+        self.open_new_window_IFD()
+    
+    def open_new_window_IFD(self):                
+        self.window = QtWidgets.QMainWindow()
+        self.ui=TY_75_Ui_MainWindow()
+        self.ui.setupUi(self.window)           
+        self.window.show()
    
     def save_test_radial(self):                     
         connection = sqlite3.connect("tyr.db")              
--- TYR_2.0_18.5/TY_50_TEST_LIST_SPECS.py
+++ TYR_2.0_18.5/TY_50_TEST_LIST_SPECS.py
@@ -5,6 +5,7 @@ from TY_05_SPECIMEN_3 import TY_05_SPECI_Ui_MainWindow
 from TY_05_SPECIMEN_4 import TY_05_SPECI_4_ui_MainWindow
 from TY_05_SPECIMEN_5 import TY_05_SPECI_5_ui_MainWindow
 from TY_05_SPECIMEN_6 import TY_05_SPECI_6_ui_MainWindow
+from TY_05_SPECIMEN_7 import TY_05_SPEC_7_Ui_MainWindow
 
 
 import sqlite3
@@ -345,6 +346,8 @@ class TY_50_LIST_Ui_MainWindow(object):
              self.open_new_window_6()
         elif(str(self.test_type_id) == "36"):    
              self.open_new_window_4()
+        elif(str(self.test_type_id) == "38"):    
+             self.open_new_window_7()
         else:
             self.open_new_window()
             print("Invalid Test ID"+str(self.test_type_id))
@@ -380,7 +383,12 @@ class TY_50_LIST_Ui_MainWindow(object):
         self.ui=TY_05_SPECI_6_ui_MainWindow()
         self.ui.setupUi(self.window)           
         self.window.show()
-        
+     
+    def open_new_window_7(self):                
+        self.window = QtWidgets.QMainWindow()
+        self.ui=TY_05_SPEC_7_Ui_MainWindow()
+        self.ui.setupUi(self.window)           
+        self.window.show()
     
    
 
